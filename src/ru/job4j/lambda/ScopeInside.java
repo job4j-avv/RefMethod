1e494ab4e8230d39516f0015f76b8c7f40a60d66
Arykin Valera <muzungu.av@gmail.com>
2021-01-05 09:34:02 +0300

    task 146183 lambda final

--- src/ru/job4j/lambda/ScopeInside.java
+++ src/ru/job4j/lambda/ScopeInside.java
@@ -5,14 +5,16 @@ import java.util.function.Supplier;
 public class ScopeInside {
     public static void main(String[] args) {
         int[] number = {10, 20, 30};
-        final int[] total = {0};
+        var ref = new Object() {
+            int total = 0;
+        };
         for (int i = 0; i < number.length; i++) {
             int num = number[i];
-            total[0] = add(
-                    () -> total[0] + num
+            ref.total = add(
+                    () -> ref.total + num
             );
         }
-        System.out.println(total[0]);
+        System.out.println(ref.total);
     }
 
     private static Integer add(Supplier<Integer> calc) {
